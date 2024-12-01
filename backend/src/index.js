62e11e22fe8e1cab4e9800f895f6e04c5ff81f01
Aditya Barnwal <adityabarnwal16@gmail.com>
2024-12-01 07:16:23 +0530

    message model and message routes created

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"; // why .js
+import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js" // why bracket    
 
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(cookieParser());
 
 const PORT=process.env.PORT
 app.use("/api/auth",authRoutes); // what is this? why api/auth
+app.use("/api/auth", messageRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port: "+ PORT);
