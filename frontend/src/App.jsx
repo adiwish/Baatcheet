2671160c7da9245862926ca11139a089353300e3
Aditya Barnwal <adityabarnwal16@gmail.com>
2024-12-09 23:40:40 +0530

    Frontend Routes Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+import Settings from "./pages/Settings"
 
+import {Routes, Route} from "react-router-dom"
 const App = () => {
   return (
-    <div className="text-red-500">Hello World</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<Settings />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
